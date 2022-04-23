a3a72d79685acf5a4d040fa0096301fc28e6d9cc
Student - Serena George <25geo02s@student.ccgs.nsw.edu.au>
2022-04-23 20:19:33 +1000

    meteor screen

--- screens/ISSLocation.js
+++ screens/ISSLocation.js
@@ -1,12 +1,48 @@
-import * as React from "react"
-import { Text, View } from "react-native"
-
-export default class ISSLocation extends React.Component {
+import * as React from 'react';
+import { Text, View, ImageBackground, SafeAreaView, Alert, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import axios from 'axios';
+export default class ISSLocationScreen extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            location: {}
+        }
+    }
+    getISSLocation = () => {
+        axios.get("https://api.wheretheiss.at/v1/satellites/25544.")
+            .then(response => { this.setState({ location: response.data }) })
+            .catch(error => { Alert.alert(error.message) })
+    }
+    componentDidMount() { this.getISSLocation() }
     render() {
-        return (
-            <View>
-                <Text> ISS Location Screen</Text>
-            </View>
-        )
+        if (Object.keys(this.state.location).length === 0) {
+            return (
+                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Text style={{ fontSize: 25 }}>Loading..
+                    </Text>
+                </View>
+            )
+        } else {
+            return (<View style={{ flex: 1 }}>
+                <SafeAreaView style={{ flex: 1 }}>
+                    <ImageBackground source={require("../assets/iss_bg.jpg")} style={{ flex: 1, resizeMode: "cover" }}>
+                        <View style={{ flex: 0.1, justifyContent: "center", alignItems: "center" }}>
+                            <Text style={{ color: "white", fontWeight: "bold", fontSize: 25 }}>ISS Location</Text>
+                        </View>
+                        <View style={{ flex: 0.7 }}>
+                            <MapView region={{ latitude: this.state.location.latitude, longitude: this.state.location.longitude, longitudeDelta: 100, latitudeDelta: 100 }}
+                                style={{ width: "100%", height: "100%" }}>
+                                <Marker coordinate={{ latitude: this.state.location.latitude, longitude: this.state.location.longitude }}>
+                                    <Image source={require("../assets/iss_icon.png")} style={{ height: 50, width: 50 }} />
+                                </Marker> </MapView> </View>
+                        <View style={{ flex: 0.2 }}>
+                            <Text> Longitude: {this.state.location.longitude} </Text>
+                            <Text> Latitude: {this.state.location.latitude} </Text>
+                            <Text> Altitude: {this.state.location.altitude} </Text>
+                            <Text> Velocity: {this.state.location.velocity} </Text>
+                        </View>
+                    </ImageBackground> </SafeAreaView> </View>)
+        }
     }
 }
--- screens/Meteor.js
+++ screens/Meteor.js
@@ -1,12 +1,51 @@
 import * as React from "react"
-import { Text, View } from "react-native"
+import { Text, View, Alert } from "react-native"
+import axios from "axios"
 
 export default class Meteor extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            meteors: {}
+        }
+    }
+    getMeteors = () => {
+        axios.get("https://api.nasa.gov/neo/rest/v1/feed?api_key=YQfcoNz7JqcPOfq8xrySs0590Ype6czB8Vim6kC6")
+            .then(response => {
+                this.setState({
+                    meteors: response.data.near_earth_objects
+                })
+                console.log(this.state.meteors)
+            })
+            .catch(error => {
+                Alert.alert(error.message)
+            })
+    }
+    componentDidMount() {
+        this.getMeteors()
+    }
     render() {
-        return (
-            <View>
-                <Text> Meteor Screen </Text>
-            </View>
-        )
+        if (Object.keys(this.state.meteors).length === 0) {
+            return (
+                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Text style={{ fontSize: 25 }}>Loading..
+                    </Text>
+                </View>
+            )
+        }
+        else {
+            let meteor_arr = Object.keys(this.state.meteors).map(meteor_date => { return this.state.meteors[meteor_date] })
+            let meteors = [].concat.apply([], meteor_arr);
+            meteors.forEach(function (element) {
+                let diameter = (element.estimated_diameter.kilometers.estimated_diameter_min + element.estimated_diameter.kilometers.estimated_diameter_max) / 2;
+                let threatScore = (diameter / element.close_approach_data[0].miss_distance.kilometers) * 1000000000;
+                element.threat_score = threatScore
+            })
+            return (
+                <View>
+                    <Text> Meteor Screen </Text>
+                </View>
+            )
+        }
     }
 }
